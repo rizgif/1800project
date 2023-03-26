a770f744e015caf118b3c1707b6e8215e24b156f
chelseaY119 <ayang106@my.bcit.ca>
2023-03-26 15:37:07 -0700

    check

--- scripts/main.js
+++ scripts/main.js
@@ -17,96 +17,13 @@ function insertNameFromFirestore() {
 
 insertNameFromFirestore();
 
-
-
-
-
-
-// displays the quote based in input param string "tuesday", "monday", etc. 
-function readQuote( day ) {
-    db.collection( "quotes" ).doc( day ).onSnapshot( doc => {
-        console.log("inside");
-        console.log( doc.data() );
-        document.getElementById( "quote-goes-here" ).innerHTML = doc.data().quote;
-    } )
-}
-// Comment out the next line (we will call this function from doAll())
-// readQuote("tuesday");       
-
-
-
-
-
-// Insert name function using the global variable "currentUser"
-function insertNameFromFirestore() {
-    currentUser.get().then(userDoc => {
-        //get the user name
-        var user_Name = userDoc.data().name;
-        console.log(user_Name);
-        $("#name-goes-here").text(user_Name); //jquery
-        // document.getElementByID("name-goes-here").innetText=user_Name;
-    })
-}
-// Comment out the next line (we will call this function from doAll())
-// insertNameFromFirestore();
-
-
-
-
-
-function writeHikes() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var hikesRef = db.collection("hikes");
-
-    hikesRef.add({
-        code: "BBY01",
-        name: "Burnaby Lake Park Trail", //replace with your own city?
-        city: "Burnaby",
-        province: "BC",
-        level: "easy",
-				details: "A lovely place for lunch walk",
-        length: 10,          //number value
-        hike_time: 60,       //number value
-        lat: 49.2467097082573,
-        lng: -122.9187029619698,
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
-    });
-    hikesRef.add({
-        code: "AM01",
-        name: "Buntzen Lake Trail", //replace with your own city?
-        city: "Anmore",
-        province: "BC",
-        level: "moderate",
-        details: "Close to town, and relaxing",
-        length: 10.5,      //number value
-        hike_time: 80,     //number value
-        lat: 49.3399431028579,
-        lng: -122.85908496766939,
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
-    });
-    hikesRef.add({
-        code: "NV01",
-        name: "Mount Seymour Trail", //replace with your own city?
-        city: "North Vancouver",
-        province: "BC",
-        level: "hard",
-        details:  "Amazing ski slope views",
-        length: 8.2,        //number value
-        hike_time: 120,     //number value
-        lat: 49.38847101455571,
-        lng: -122.94092543551031,
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
-    });
-}
-
-insertNameFromFirestore();
-
 // fraser health
 // Get a reference to the button by its ID
 var button1 = document.getElementById('confirm-fh');
 
 // Add a click event listener to the button
 button1.addEventListener('click', function() {  
+  console.log("button clicked");
 // Create a new object with the data to be added to the database
 var data = {
   code: "fraserhealth",
