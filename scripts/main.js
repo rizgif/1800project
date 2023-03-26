4b5c1817ed6625d1ef3c20c37b844f235f4863e6
chelseaY119 <ayang106@my.bcit.ca>
2023-03-26 15:08:35 -0700

    add new nav design

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 function insertNameFromFirestore() {
-    //check if user is logged in
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) { //if user logged in
-            console.log(user.uid)
-            db.collection("users").doc(user.uid).get().then(userDoc => {
-                // console.log(userDoc.data().name)
-                userName = userDoc.data().name;
-                // console.log(userName)
-                document.getElementById("name-goes-here").innerHTML = userName;
+  //check if user is logged in
+  firebase.auth().onAuthStateChanged(user => {
+      if (user) { //if user logged in
+          console.log(user.uid)
+          db.collection("users").doc(user.uid).get().then(userDoc => {
+              // console.log(userDoc.data().name)
+              userName = userDoc.data().name;
+              // console.log(userName)
+              document.getElementById("name-goes-here").innerHTML = userName;
 
-            })
-        }
-    })
+          })
+      }
+  })
 
 }
 
@@ -23,27 +23,27 @@ var button1 = document.getElementById('confirm-fh');
 
 // Add a click event listener to the button
 button1.addEventListener('click', function() {  
-  // Create a new object with the data to be added to the database
-  var data = {
-    code: "fraserhealth",
-    locationName: "Fraser Health"
-    // add more data fields as needed
-  };
+// Create a new object with the data to be added to the database
+var data = {
+  code: "fraserhealth",
+  locationName: "Fraser Health"
+  // add more data fields as needed
+};
 
-  // Get a reference to the "locations" collection in Firebase
-  var locationsRef = firebase.firestore().collection("locations");
+// Get a reference to the "locations" collection in Firebase
+var locationsRef = firebase.firestore().collection("locations");
 
-  // Add the new data to the "locations" collection
-  locationsRef.add(data)
-    .then(function(docRef) {
-      window.location.href = 'eachhike.html';
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
+// Add the new data to the "locations" collection
+locationsRef.add(data)
+  .then(function(docRef) {
+    window.location.href = 'eachhike.html';
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
 
-    // window.location.href = 'eachhike.html';
+  // window.location.href = 'eachhike.html';
 });
 
 
@@ -53,26 +53,26 @@ var button2 = document.getElementById('comfirm-inter');
 
 // Add a click event listener to the button
 button2.addEventListener('click', function() {  
-  // Create a new object with the data to be added to the database
-  var data = {
-    code: "interiorhealth",
-    locationName: "Interior Health"
-    // add more data fields as needed
-  };
+// Create a new object with the data to be added to the database
+var data = {
+  code: "interiorhealth",
+  locationName: "Interior Health"
+  // add more data fields as needed
+};
 
-  // Get a reference to the "locations" collection in Firebase
-  var locationsRef = firebase.firestore().collection("locations");
-  // Add the new data to the "locations" collection
-  locationsRef.add(data)
-    .then(function(docRef) {
-      window.location.href = 'eachhike.html';
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
+// Get a reference to the "locations" collection in Firebase
+var locationsRef = firebase.firestore().collection("locations");
+// Add the new data to the "locations" collection
+locationsRef.add(data)
+  .then(function(docRef) {
+    window.location.href = 'eachhike.html';
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
 
-    // window.location.href = 'eachhike.html';
+  // window.location.href = 'eachhike.html';
 });
 
 
@@ -81,26 +81,26 @@ var button3 = document.getElementById('comfirm-island');
 
 // Add a click event listener to the button
 button3.addEventListener('click', function() {  
-  // Create a new object with the data to be added to the database
-  var data = {
-    code:"islandhealth",
-    locationName: "Island Health"
-    // add more data fields as needed
-  };
+// Create a new object with the data to be added to the database
+var data = {
+  code:"islandhealth",
+  locationName: "Island Health"
+  // add more data fields as needed
+};
 
-  // Get a reference to the "locations" collection in Firebase
-  var locationsRef = firebase.firestore().collection("locations");
-  // Add the new data to the "locations" collection
-  locationsRef.add(data)
-    .then(function(docRef) {
-      window.location.href = 'eachhike.html';
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
+// Get a reference to the "locations" collection in Firebase
+var locationsRef = firebase.firestore().collection("locations");
+// Add the new data to the "locations" collection
+locationsRef.add(data)
+  .then(function(docRef) {
+    window.location.href = 'eachhike.html';
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
 
-    // window.location.href = 'eachhike.html';
+  // window.location.href = 'eachhike.html';
 });
 
 
@@ -109,26 +109,26 @@ var button4 = document.getElementById('comfirm-nh');
 
 // Add a click event listener to the button
 button4.addEventListener('click', function() {  
-  // Create a new object with the data to be added to the database
-  var data = {
-    code:"northernhealth",
-    locationName: "Northern Health"
-    // add more data fields as needed
-  };
+// Create a new object with the data to be added to the database
+var data = {
+  code:"northernhealth",
+  locationName: "Northern Health"
+  // add more data fields as needed
+};
 
-  // Get a reference to the "locations" collection in Firebase
-  var locationsRef = firebase.firestore().collection("locations");
-  // Add the new data to the "locations" collection
-  locationsRef.add(data)
-    .then(function(docRef) {
-      window.location.href = 'eachhike.html';
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
+// Get a reference to the "locations" collection in Firebase
+var locationsRef = firebase.firestore().collection("locations");
+// Add the new data to the "locations" collection
+locationsRef.add(data)
+  .then(function(docRef) {
+    window.location.href = 'eachhike.html';
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
 
-    // window.location.href = 'eachhike.html';
+  // window.location.href = 'eachhike.html';
 });
 
 
@@ -137,26 +137,26 @@ var button5 = document.getElementById('comfirm-vch');
 
 // Add a click event listener to the button
 button5.addEventListener('click', function() {  
-  // Create a new object with the data to be added to the database
-  var data = {
-    code:"vancouver",
-    locationName: "Vancouver Coastal Health"
-    // add more data fields as needed
-  };
+// Create a new object with the data to be added to the database
+var data = {
+  code:"vancouver",
+  locationName: "Vancouver Coastal Health"
+  // add more data fields as needed
+};
 
-  // Get a reference to the "locations" collection in Firebase
-  var locationsRef = firebase.firestore().collection("locations");
-  // Add the new data to the "locations" collection
-  locationsRef.add(data)
-    .then(function(docRef) {
-      window.location.href = 'eachhike.html';
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
+// Get a reference to the "locations" collection in Firebase
+var locationsRef = firebase.firestore().collection("locations");
+// Add the new data to the "locations" collection
+locationsRef.add(data)
+  .then(function(docRef) {
+    window.location.href = 'eachhike.html';
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
 
-    
+  
 });
 
 
