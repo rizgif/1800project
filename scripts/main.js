c784efef9e107fb9531fb9bd1ad85a5cb20c8926
chelseaY119 <ayang106@my.bcit.ca>
2023-04-01 14:12:08 -0700

    checked

--- scripts/main.js
+++ scripts/main.js
@@ -145,7 +145,7 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('a').href = "eachLocation.html?docID=" + docId;
                 console.log(docId)
 
-                localStorage.setItem("docId", docId);
+                // localStorage.setItem("docId", docId);
                 
 
                 //NEW LINE: update to display length, duration, last updated
