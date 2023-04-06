95a1afab79d43fe2292c6c056c71a36b95d99b14
“Riz” <rnursaidy@gmail.com>
2023-04-06 15:30:15 -0700

    cleaned up the code

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,39 +1,28 @@
 // Run all this to figure out where the user currently is
 var locationID;
 function getCurrentLocation() {
-
   //get geolocation of the user
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
       console.log(latitude, " ", longitude);
-      // sendPosition(position); // call sendPosition() with latitude and longitude values
 
       //decide which health authority the user belongs to
-      //the first one is corresponding to bcit location. 
+
       if (latitude > 49 && longitude < -123) {
         locationID = "7TVhl0XTvb17Kj7e2VNa";
         console.log("fraserhealth", locationID);
-        
-      }
-
-      else if (latitude > 50 && longitude < -124) {
+      } else if (latitude > 50 && longitude < -124) {
         locationID = "svN4aQvBZDPG4CYbPQJR";
         console.log("islandhealth");
-      }
-
-      else if (latitude > 51 && longitude < -125) {
+      } else if (latitude > 51 && longitude < -125) {
         locationID: "i9k0csjI8D4j1qO5SRBh";
         console.log("northernhealth");
-      }
-
-      else if (latitude > 52 && longitude < -126) {
+      } else if (latitude > 52 && longitude < -126) {
         locationID: "QxuzllDeJsANXlK485jy";
         console.log("vancouverhealth");
-      }
-
-      else if (latitude > 53 && longitude < -130) {
+      } else if (latitude > 53 && longitude < -130) {
         locationID: "eGT8jGFAvzKAhO5du1Rz";
         console.log("interiorhealth");
       }
@@ -43,15 +32,6 @@ function getCurrentLocation() {
   } else {
     alert("Geolocation is not supported by this browser.");
   }
-
-  //if lat and lng within some bodary
-
-
-  //    id = "7Tlkdjaudfois"
-  //else if ....
-  //    id = sndljafldsf
-
-  // locationID = id;
 }
 getCurrentLocation();
 
@@ -70,22 +50,27 @@ var uiConfig = {
       // If the user is a "brand new" user, then create a new "user" in your own database.
       // Assign this user with the name and email provided.
       // Before this works, you must enable "Firestore" from the firebase console.
-      // The Firestore rules must allow the user to write. 
+      // The Firestore rules must allow the user to write.
       //------------------------------------------------------------------------------------------
-      var user = authResult.user;                            // get the user object from the Firebase authentication database
-      if (authResult.additionalUserInfo.isNewUser) {         //if new user
-        db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
-          name: user.displayName,                    //"users" collection
-          email: user.email,                         //with authenticated user's ID (user.uid)
-          country: "Canada",                      //optional default profile info      
-          school: "BCIT"                          //optional default profile info
-        }).then(function () {
-          // 
-          console.log("New user added to firestore:" + user.displayName);
-          localStorage.setItem('userName', user.displayName);
+      var user = authResult.user; // get the user object from the Firebase authentication database
+      if (authResult.additionalUserInfo.isNewUser) {
+        //if new user
+        db.collection("users")
+          .doc(user.uid)
+          .set({
+            //write to firestore. We are using the UID for the ID in users collection
+            name: user.displayName,
+            email: user.email,
+            country: "Canada",
+            school: "BCIT",
+          })
+          .then(function () {
+            //
+            console.log("New user added to firestore:" + user.displayName);
+            localStorage.setItem("userName", user.displayName);
 
-          window.location.assign('eachLocation.html?ID=' + locationID);       //re-direct to main.html after signup
-        })
+            window.location.assign("eachLocation.html?ID=" + locationID); //re-direct to main.html after signup
+          })
           .catch(function (error) {
             console.log("Error adding new user: " + error);
           });
@@ -97,27 +82,17 @@ var uiConfig = {
     uiShown: function () {
       // The widget is rendered.
       // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
+      document.getElementById("loader").style.display = "none";
+    },
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: 'eachLocation.html?ID=' + locationID,
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
-  ],
+  signInFlow: "popup",
+  signInSuccessUrl: "eachLocation.html?ID=" + locationID,
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
   // Terms of service url.
-  tosUrl: '<your-tos-url>',
+  tosUrl: "<your-tos-url>",
   // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
+  privacyPolicyUrl: "<your-privacy-policy-url>",
 };
 
-ui.start('#firebaseui-auth-container', uiConfig);
-
-
+ui.start("#firebaseui-auth-container", uiConfig);
