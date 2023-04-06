cbf3e9ff8d3c317346635fea14388ddcd89feb4c
“Riz” <rnursaidy@gmail.com>
2023-04-05 20:47:52 -0700

    user profile changes

--- scripts/profile.js
+++ scripts/profile.js
@@ -11,23 +11,23 @@ function populateUserInfo() {
             currentUser.get()
                 .then(userDoc => {
                     //get the data fields of the user
-                    var country = userDoc.data().country;
-                    var email = userDoc.data().email;
-                    var name = userDoc.data().name;
-                    var school = userDoc.data().school;
+                    var country1 = userDoc.data().country;
+                    var email1 = userDoc.data().email;
+                    var name1 = userDoc.data().name;
+                    var school1 = userDoc.data().school;
 
                     //if the data fields are not empty, then write them in to the form.
-                    if (userName != null) {
-                        document.getElementById("country").value = country;
+                    if (country1 != null) {
+                        document.getElementById("countryinput").value = country1;
                     }
-                    if (userSchool != null) {
-                        document.getElementById("email").value = email;
+                    if (email1 != null) {
+                        document.getElementById("emailinput").value = email1;
                     }
-                    if (userCity != null) {
-                        document.getElementById("name").value = name;
+                    if (name1 != null) {
+                        document.getElementById("nameinput").value = name1;
                     }
-                    if (userCity != null) {
-                        document.getElementById("school").value = school;
+                    if (school1 != null) {
+                        document.getElementById("schoolinput").value = school1;
                     }
                 })
         } else {
@@ -49,17 +49,17 @@ function saveUserInfo() {
     //enter code here
 
     //a) get user entered values
-    country = document.getElementById('country').value;       //get the value of the field with id="nameInput"
-    email = document.getElementById('email').value;     //get the value of the field with id="schoolInput"
-    name = document.getElementById('name').value;       //get the value of the field with id="cityInput"
-    school = document.getElementById('school').value;
+    country1 = document.getElementById('countryinput').value;       //get the value of the field with id="nameInput"
+    email1 = document.getElementById('emailinput').value;     //get the value of the field with id="schoolInput"
+    name1 = document.getElementById('nameinput').value;       //get the value of the field with id="cityInput"
+    school1 = document.getElementById('schoolinput').value;
 
     //b) update user's document in Firestore
     currentUser.update({
-        country: country,
-        email: email,
-        name: name,
-        school: school
+        country: country1,
+        email: email1,
+        name: name1,
+        school: school1
 
     })
     .then(() => {
